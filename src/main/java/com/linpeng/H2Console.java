0e23c38648a887ee50e8fb337318f6d007790657
magixsource <magixsource.cn@gmail.com>
2018-01-25 23:50:07 +0800

    H2 Console

---
+++ src/main/java/com/linpeng/H2Console.java
@@ -0,0 +1,20 @@
+package com.linpeng;
+
+/**
+ * H2 console
+ * 
+ * @author linpeng
+ *
+ */
+public class H2Console {
+
+	// public static void startDB() throws SQLException {
+	// Server.createTcpServer("-tcpPort", "9092", "-tcpAllowOthers").start();
+	// String[] args = {"-web","-browser"};
+	// Console.main(args);
+	// }
+	//
+	// public static void stopDB() throws SQLException {
+	// Server.shutdownTcpServer("tcp://127.0.0.1:9092", "", true, true);
+	// }
+}
